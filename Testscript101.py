d4e5f5155cd7b3601bf64ff7932296975c873858
bchoi0000 <64298615+bchoi0000@users.noreply.github.com>
2020-09-14 10:57:32 -0700

    Create Testscript101.py

---
+++ Testscript101.py
@@ -0,0 +1,22 @@
+class testclass:
+
+    def __init__(self, y, z):
+        self.f = y*3
+        self.g = z*2
+        #print(self.f)
+        #print(self.g)
+
+class childclass(testclass):
+    def __init__(self, a, b):
+        #self.a = a
+        #self.b = b
+        super().__init__(a,b)
+        print(self.f)
+        print(self.g)
+
+class child2class(testclass):
+    def __init__(self, c, d):
+        pass
+
+#obj1 = testclass(1,2)
+obj2 = childclass(10,20)
